1e65f239f0e94c9c4b3a5e272223f311016d31d4
Gabriel Sousa <116306408+Gabriel-Sous-a@users.noreply.github.com>
2023-10-15 22:28:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const app = express();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const githubWebhookHandler = require('github-webhook-handler');
@@ -7,12 +9,12 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN); 
+client.login(process.env.DISCORD_BOT_TOKEN);
 
 handler.on('pull_request', function (event) {
   const pr = event.payload.pull_request;
   const message = `New pull request opened: ${pr.title} by ${pr.user.login}\n${pr.html_url}`;
-  const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID); 
+  const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
   if (channel) {
     channel.send(message);
   } else {
@@ -20,12 +22,13 @@ handler.on('pull_request', function (event) {
   }
 });
 
-const http = require('http');
-http.createServer(function (req, res) {
+app.post('/webhook', (req, res) => {
   handler(req, res, function (err) {
     res.statusCode = 404;
     res.end('no such location');
   });
-}).listen(7777, () => {
+});
+
+const server = app.listen(7777, () => {
   console.log('Server running at http://localhost:7777/');
 });
