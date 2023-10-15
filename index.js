f75887771c0e29c2a3383f0ad7f0a5a1e4d6b3ad
Gabriel Sousa <116306408+Gabriel-Sous-a@users.noreply.github.com>
2023-10-15 23:02:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// index.js
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const githubWebhookHandler = require('github-webhook-handler');
@@ -8,26 +6,29 @@ const handler = githubWebhookHandler({ path: '/api/webhook', secret: process.env
 module.exports = (req, res) => {
   if (req.method === 'POST') {
     handler(req, res, function (err) {
-      res.status(404).send('no such location');
+      if (err) {
+        console.error('Error in handling GitHub webhook:', err.message);
+        res.status(500).send('Internal Server Error');
+      }
     });
   } else {
     res.status(405).send('Method Not Allowed');
   }
-  
-  client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
-
-  client.login(process.env.DISCORD_BOT_TOKEN);
-
-  handler.on('pull_request', function (event) {
-    const pr = event.payload.pull_request;
-    const message = `New pull request opened: ${pr.title} by ${pr.user.login}\n${pr.html_url}`;
-    const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
-    if (channel) {
-      channel.send(message);
-    } else {
-      console.error('The channel does not exist!');
-    }
-  });
 };
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+handler.on('pull_request', function (event) {
+  const pr = event.payload.pull_request;
+  const message = `New pull request opened: ${pr.title} by ${pr.user.login}\n${pr.html_url}`;
+  const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
+  if (channel) {
+    channel.send(message);
+  } else {
+    console.error('The channel does not exist!');
+  }
+});
