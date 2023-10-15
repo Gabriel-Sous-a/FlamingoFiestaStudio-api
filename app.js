cd626e3e780becc79a6514d911b9f49831bdf791
Gabriel Sousa <116306408+Gabriel-Sous-a@users.noreply.github.com>
2023-10-15 20:06:45 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const githubWebhookHandler = require('github-webhook-handler');
+const handler = githubWebhookHandler({ path: '/webhook', secret: process.env.GITHUB_WEBHOOK_SECRET });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN); 
+
+handler.on('pull_request', function (event) {
+  const pr = event.payload.pull_request;
+  const message = `New pull request opened: ${pr.title} by ${pr.user.login}\n${pr.html_url}`;
+  const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID); 
+  if (channel) {
+    channel.send(message);
+  } else {
+    console.error('The channel does not exist!');
+  }
+});
+
+const http = require('http');
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404;
+    res.end('no such location');
+  });
+}).listen(7777, () => {
+  console.log('Server running at http://localhost:7777/');
+});
